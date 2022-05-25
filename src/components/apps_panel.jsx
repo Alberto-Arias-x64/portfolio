70d5d987947c0ee8e4a00aac34fdd8fd00a482de
x64 <alberto_arias_x64@gmail.com>
2022-05-25 01:38:26 -0500

    post build

--- src/components/apps_panel.jsx
+++ src/components/apps_panel.jsx
@@ -8,7 +8,7 @@ const Control_panel = () => {
         <div id='control_grid'>
             <div id="control">
                 <div className='app active_icon'><Copy scale={0.65} /></div>
-                <div onClick={event => redir(event, 'https://www.linkedin.com/in/nicolas-alberto-arias-otalora-629128211/')} className='app'><Linkeding scale={0.85} /></div>
+                <div onClick={event => redir(event, 'https://www.linkedin.com/in/alberto-arias-629128211/')} className='app'><Linkeding scale={0.85} /></div>
                 <div onClick={event => redir(event, 'https://github.com/Alberto-Arias-x64')} className='app'><Branch scale={0.65} /></div>
                 <div onClick={event => redir(event, 'https://alberto-arias-x64.github.io/Pagina-web/')} className='app'><Debug scale={0.62} /></div>
                 <div onClick={event => redir(event, 'https://mail.google.com/mail/u/0/?view=cm&fs=1&to=alberto.arias.x64@gmail.com&tf=1')} className='app'  ><Mail scale={1} /></div>
